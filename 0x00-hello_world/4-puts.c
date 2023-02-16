ac6d9334f7e63807eb952a7272b74ebfd6246ef7
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-02-16 15:41:27 -0800

    print some stuff in c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Simply prints out some strings in the stream and returns 0
+ * 
+ * Return: always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);	
+	return (0);
+}
