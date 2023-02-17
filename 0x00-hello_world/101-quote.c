15a29f1602ec5f31fce31fe1eb5ecf2629ea7ba2
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-02-16 17:04:15 -0800

    My error prone code

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - The main function is the entry point
+ *
+ * Return: will be 1
+ *
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, ");
+	fprintf(stderr, "2015-10-19\n");
+	return (1);
+}
