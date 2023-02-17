5b195e6ee784c58c969ddb1e6a2af009c82107f2
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-02-17 13:58:19 -0800

    iterate and print every lowercase letter

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - The main function of the programe
+ *
+ * Return: aways return 0
+ */
+
+int main(void)
+{
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
