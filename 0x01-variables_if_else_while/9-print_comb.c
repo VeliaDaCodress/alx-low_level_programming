b4ea5fddb78cfa477f98a9d2a3b2c8b9ab95c4ee
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-02-17 20:03:07 -0800

    print numberz

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,11 +9,17 @@
 int main(void)
 {
 	int i;
+	int j;
 
 	for (i = 0; i < 10; i++)
 	{
-		putchar(i + '0');
+		for (j = 0; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(',');
+			putchar(j + '0');
+			putchar(' ');
+		}
 	}
-	putchar('\n');
 	return (0);
 }
