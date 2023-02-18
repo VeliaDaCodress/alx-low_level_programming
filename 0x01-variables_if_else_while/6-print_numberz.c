48de6b3bbecc64999fea4f040b98698f3dadda72
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-02-17 19:06:53 -0800

    Only single digit numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - The main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	while (i < 10)
+	{
+		putchar(i++);
+	}
+	printf("\n");
+	return (0);
+}
