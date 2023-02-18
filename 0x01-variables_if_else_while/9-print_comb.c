b1ad43fbe5e40671aa56e0256b07011fd47e2f64
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-02-17 20:16:26 -0800

    Combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,15 +9,15 @@
 int main(void)
 {
 	int i;
-	int j;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		putchar(i + '0');
+		if (i == 9)
+			putchar(' ');
+		else
 		{
-			putchar(i + '0');
 			putchar(',');
-			putchar(j + '0');
 			putchar(' ');
 		}
 	}
