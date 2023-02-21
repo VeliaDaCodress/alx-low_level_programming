761953ec7d6e6915777424fb3df9b7b2f14b0642
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-02-21 14:21:02 -0800

    whats my last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_last_digit - this function puts the last digit on the screen
+ *
+ * @a: parameter for function
+ *
+ * Return: the last man standing
+ *
+ */
+
+int print_last_digit(int a)
+{
+	int last_digit;
+
+	last_digit = a % 10;
+	if (last_digit < 0)
+		last_digit = -(last_digit);
+	else
+		last_digit = last_digit;
+	return (last_digit);
+}
