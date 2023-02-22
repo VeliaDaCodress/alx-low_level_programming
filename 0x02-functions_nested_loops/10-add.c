3e3f290762b31ba8818754bb3d405341fe124f01
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-02-22 03:24:24 -0800

    The adder is a very strong and deadly snake

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - The main function that adds two integers
+ *
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: integer value of the sum
+ *
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
