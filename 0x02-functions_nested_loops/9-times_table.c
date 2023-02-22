0701525afb0ab4b03160e303bebbfe92e6ad6ad1
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-02-22 03:05:01 -0800

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * times_table - This function prints the times table
+ *
+ */
+
+void times_table(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		int j = 0;
+
+		while (j <= 9)
+		{
+			int multi;
+
+			multi = i * j;
+			if (multi / 10 == 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(multi + '0');
+				_putchar(',');
+			}
+			else
+			{
+				int first_digit, last_digit;
+
+				first_digit = multi / 10;
+				last_digit = multi % 10;
+				_putchar(' ');
+				_putchar(first_digit + '0');
+				_putchar(last_digit + '0');
+				_putchar(',');
+			}
+			j++;
+		}
+		_putchar('$');
+		_putchar('\n');
+		i++;
+	}
+}
