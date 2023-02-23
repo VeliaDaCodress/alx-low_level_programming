77bb7bb521ba50bcfc9ba5a5f89b940632a0ae1c
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-02-23 11:17:53 -0800

    what is a in b places

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - this function multiplies two numbers and returns a value
+ *
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: integer value of a times b
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c  = a * b;
+	return (c);
+}
