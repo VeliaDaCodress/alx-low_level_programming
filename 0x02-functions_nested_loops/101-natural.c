d2e7c4444b180ec6367883f05c4917c577ad9007
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-02-22 17:01:37 -0800

    multiples of 3 or 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,6 +3,8 @@
 /**
  * main - This function prints the sum of all naturals
  *
+ *
+ * Return: always 0
  */
 
 int main(void)
@@ -12,7 +14,7 @@ int main(void)
 
 	for (i = 1; i < 1024; i++)
 	{
-		if (i % 3 || i % 5)
+		if (i % 3 == 0 || i % 5 == 0)
 			sum = sum + i;
 	}
 	printf("%d\n", sum);
