6106ebbf3206f5a12db6410d7d4d1b12cf1c90f7
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-02-24 03:24:16 -0800

    diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - This function printa diagonals
+ *
+ * @n: parameter representing how many levels of diagonal to print
+ */
+
+void print_diagonal(int n)
+{
+	int i = 0;
+
+	while (i <= n)
+	{
+		int j;
+
+		for (j = 1; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		if (i > 0)
+		{
+			_putchar('\\');
+			_putchar('\n');
+		}
+		i++;
+	}
+}
