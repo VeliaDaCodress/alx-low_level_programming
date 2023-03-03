dfe2df3891f745c9ad0bad525ca222669a3f3678
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-03-02 19:15:42 -0800

    concatenate two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - This fnc contlcatenates two strings
+ *
+ * @dest: first string
+ * @src: second string
+ *
+ * Return: the new combined words
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int dest_len = 0;
+	int src_len = 0;
+	int i;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		dest_len++;
+	for (i = 0; src[i] != '\0'; i++)
+		src_len++;
+	for (i = 0; i <= src_len; i++)
+	{
+		dest[src_len + i] = src[i];
+	}
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+
+/**
+ * _putchar - It prints the char c to stdout
+ * @c: the character to be sent to the stdout
+ *
+ * Return: On success 1, on error -1 
+ */
+
+
