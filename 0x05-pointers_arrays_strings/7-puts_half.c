77f3a4e6bd144a2f5496d0eef91aacab127d7017
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-03-06 10:42:30 -0800

    correction

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,9 +13,8 @@ void puts_half(char *str)
 	length = 0;
 	for (i = 0; str[i] != '\0'; i++)
 		length++;
-	if (length % 2 == 0)
-		half_length = length / 2;
-	else
+	half_length =  length / 2;
+	if ((length % 2) == 1)
 		half_length = (length - 1) / 2;
 	for (i = half_length; str[i]; i++)
 		_putchar(str[i]);
