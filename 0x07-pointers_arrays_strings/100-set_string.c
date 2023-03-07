3000dd951a6d7e94681bb46efd1be5ed27513f17
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-03-06 16:38:48 -0800

    set pointer to pointer to char value

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets the valie of a pointer to a char
+ * @s: pointer to pointer
+ * @to: pointer to chat
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
