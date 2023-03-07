1e1621a1faca2e064242e26b732962feaa07a168
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-03-07 14:46:11 -0800

    set the memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,7 +10,7 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	int i = 0;
 
 	for (; n > 0; i++)
 	{
@@ -19,3 +19,4 @@ char *_memset(char *s, char b, unsigned int n)
 	}
 	return (s);
 }
+
