3113c89357b6671253187b9a3c5517154c35e594
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-03-08 06:04:43 -0800

    reversal with recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recur - Print the characters in reverse
+ * @s: the string to print in reverse
+ */
+
+void _print_rev_recursion(char *s)
+{
+	int lengt;
+
+	lengt = strlen(s);
+	if (!lengt)
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*(s + (lengt - 1)));
+	_print_rev_recursion(char s + (lengt - 1));
+}
