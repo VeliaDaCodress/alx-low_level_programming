a3f27f051cb7fe4e92c3b46506201f54d7d4f081
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-03-14 16:56:37 -0700

    strings

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,9 +14,9 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *arr;
 
+	arr = malloc(sizeof(char) * size);
 	if (size == 0 || *c == NULL)
 		return (NULL);
-	arr = (char *) malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
