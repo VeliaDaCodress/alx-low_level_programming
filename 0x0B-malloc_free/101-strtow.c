4f3e651e8d0a47239c7abe1091723dbccd28601d
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-03-15 14:25:59 -0700

    strtow task

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,8 +9,8 @@
 
 int count_word(char *s)
 {
-       	int flag, c, w;
-	
+	int flag, c, w;
+
 	flag = 0;
 	w = 0;
 	for (c = 0; s[c] != '\0'; c++)
@@ -36,7 +36,7 @@ char **strtow(char *str)
 {
 	char **matrix, *tmp;
 	int i, k = 0, len = 0, words, c = 0, start, end;
-	
+
 	while (*(str + len))
 		len++;
 	words = count_word(str);
