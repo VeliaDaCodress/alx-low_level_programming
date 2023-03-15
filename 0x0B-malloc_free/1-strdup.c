8f20731de4411cb9d9165c0131e68ca1a8cf22a8
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-03-14 17:09:32 -0700

    resolving strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,13 +14,13 @@ char *_strdup(char *str)
 	int i, count;
 	char *mem_ptr;
 
+	if (str == NULL)
+		retuen (NULL);
 	count = 0;
 	while (str[count] != '\0')
 		count++;
-	if (count == 0 || *str == NULL)
-		return (NULL);
-	mem_ptr = malloc(sizeof(char) * (count + 1));
-	if (mem_ptr == NULL)
+	mem_ptr = malloc(sizeof(chat) * (count + 1));
+	if (count == 0 || mem_ptr == NULL)
 		return (NULL);
 	for (i = 0; str[i]; i++)
 		mem_ptr[i] = str[i];
