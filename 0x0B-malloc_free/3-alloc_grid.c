cf7f945029dcb8e3d7e15de727d78afc32f864b8
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-03-15 11:52:25 -0700

    alloc grid task

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -32,7 +32,7 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
-			dummy_grid[i][j] = 0
+			dummy_grid[i][j] = 0;
 	}
 	return (dummy_grid);
 }
