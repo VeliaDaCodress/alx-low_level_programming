f7402fe5ce8424853dfe6d43966b718d9f87b987
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-03-20 08:41:14 -0700

    Print out the name of the file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define __FILE__
+/**
+ * main - Main code of the function
+ * Return: 0 always successful
+ */
+
+int main(void)
+{
+	printf("%s", FILE);
+	return (0);
+}
