4a9183cc68458e2989fc8c791f76dbdfac515d54
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-03-21 07:38:01 -0700

    what is a dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Creating a new structure for dogs
+ * @name: The name of the dog
+ * @age: how old is the dog
+ * @owner: who owns the dog
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
