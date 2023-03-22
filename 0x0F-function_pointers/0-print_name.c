06d57cf7c8be9d450a3fc9c829e4d6525d859d59
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-03-22 15:50:20 -0700

    Try to print

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - A function that prints a name out
+ * @name: the name to print
+ * @f: pointer to function of the printer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
+
+/**
+ * main - entry point
+ * Return: always 0
+ */
+
+int main(void)
+{
+	print_name("joseph", print_name_as_is());
+	print_name("joseph", print_name_uppercase());
+	return (0);
+}
