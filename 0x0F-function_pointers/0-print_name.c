ff2427b75cd18069ea762218a8c886053deaf0f1
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-03-22 21:48:07 -0700

    whatsa wi ya

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,11 +5,12 @@
  * print_name - A function that prints a name out
  * @name: the name to print
  * @f: pointer to function of the printer
+ * @*:parameter
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL))
+	if (name == NULL || f == NULL)
 		return;
 	f(name);
 }
