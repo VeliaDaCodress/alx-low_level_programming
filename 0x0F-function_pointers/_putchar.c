c52e4aa8e54a3cb59387d10a2349e4ec4011b309
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-03-24 14:25:08 -0700

    putchar

--- 0x0F-function_pointers/_putchar.c
+++ 0x0F-function_pointers/_putchar.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
- * _putchar - Print out characters
+ * putchar - Print out characters
  * @c: char to print out
  * Return: always 0
  */
 
-int _putchar(char c)
+int putchar(char c)
 {
 	write(1, &c, 1);
 	return (0);
