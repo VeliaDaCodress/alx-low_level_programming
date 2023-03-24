b1fc5544f0031383bd973fa6362772f9eb9f9edb
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-03-24 14:46:51 -0700

    The print name retried again

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,9 +2,12 @@
 
 /**
  * print_name - function that calls printer
+ * @name: the name you want to print
+ * @f: function pointer
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (f && name)
+		f(name);
 }
