0e55a80819978c5259409da8753f4dd15a75c76c
jossyboy2580 <joseph4hacking@gmail.com>
2023-03-29 17:01:05 -0700

    Variadic number one

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * sum_them_all - This function sums up all thw arguments
+ * n: the xount of the numbers
+ * Return: integer value of the sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum, i;
+	va_list va_l;
+
+	sum = 0;
+	va_start(n, va_l);
+	for (i = 0; i < n; i++)
+		sum += va_arg;
+	va_end;
+	return sum;
+}
