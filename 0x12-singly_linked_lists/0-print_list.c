a1ead016520e8dde99662fe6c06fe2a8159778aa
jossyboy2580 <joseph4hacking@gmail.com>
2023-03-31 16:50:04 -0700

    corrected some

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,10 @@
 #include "lists.h"
 
-
+/**
+ * print_list - prints all the elements of a linked list
+ * @h: pointer to the list_t list to print
+ * Return: size of the linked tree
+ */
 
 size_t print_list(const list_t *h)
 {
@@ -9,11 +13,16 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (!h->str)
+		{
 			printf("[0] (nil)");
+			s += sizeof(*h);
+		}
 		else
+		{
 			printf("[%u] %s", h->len, h->str);
+			s += sizeof(*h);
+		}
 		h = h->next;
-		s++;
 	}
 	return (s);
 }
