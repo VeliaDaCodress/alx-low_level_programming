4c4af01b13b58176ae3e023b33d6b9888b477c6e
jossyboy2580 <joseph4hacking@gmail.com>
2023-04-02 01:59:38 -0700

    this definitely checks

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,7 +7,7 @@
  * Return: integer value of the length of the string
  */
 
-int _strlen(char *str)
+int _strlen(const char *str)
 {
 	int n;
 
@@ -26,13 +26,12 @@ int _strlen(char *str)
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *old_head;
-	int n = _strlen(str);
 
 	old_head = malloc(sizeof(list_t));
 	if (old_head == NULL)
 		return (NULL);
 	old_head->str = strdup(str);
-	old_head->len = n;
+	old_head->len = _strlen(str);
 	old_head->next = *head;
 	*head = old_head;
 	return (old_head);
