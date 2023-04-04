04f0c91dbf2ed94abe797d9b800f85f7c6e151aa
jossyboy2580 <joseph4hacking@gmail.com>
2023-04-04 15:07:15 -0700

    get node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,6 +4,7 @@
  * get_nodeint_at_index - return node at index
  * @head: pointer to the head of linked list
  * @index: position of the node we want to return
+ * Return: the node at index index
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
