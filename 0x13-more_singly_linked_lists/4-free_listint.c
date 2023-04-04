0e97856de3915becf6f16b4153704e6869358f39
jossyboy2580 <joseph4hacking@gmail.com>
2023-04-04 02:24:29 -0700

    free the list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - free the list 
+ * @head: the head of the list
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *node;
+
+	while (head)
+	{
+		node = head->next;
+		free(head->n);
+		free(head);
+		head = node;
+	}
+}
