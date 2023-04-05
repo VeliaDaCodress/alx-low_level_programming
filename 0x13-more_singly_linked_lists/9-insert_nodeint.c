06908e4be973560e7f2c17f4a408e980bd026e24
jossyboy2580 <joseph4hacking@gmail.com>
2023-04-05 07:25:15 -0700

    insert a node at idx

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - return node at index
+ * @head: pointer to the head of linked list
+ * @index: position of the node we want to return
+ * Return: the node at index index
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int count;
+	listint_t *temp = head;
+
+	count = 0;
+	while (count < index && temp)
+	{
+		temp = temp->next;
+		count++;
+	}
+	return (temp ? temp : NULL);
+}
+
+/**
+ * insert_nodeint_at_index - insert nodeint at index
+ * @head: head of the linked list
+ * @idx: the index we want to add the node to
+ * @n: the n value of the data field
+ * Return: pointer to new node or NULL if it failed
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new;
+	listint_t *prev_node;
+
+	prev_node = get_nodeint_at_index(*head, (idx - 1));
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = prev_node->next;
+	prev_node->next = new;
+	return (new);
+}
