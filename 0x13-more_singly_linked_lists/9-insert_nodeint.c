1d5e23e38cca24c94e3e7cb9f9ff16951f431743
jossyboy2580 <joseph4hacking@gmail.com>
2023-04-05 15:55:08 -0700

    add to index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,17 +33,21 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new;
 	listint_t *prev_node;
 
-	prev_node = get_nodeint_at_index(*head, (idx - 1));
+	if (!head || !(*head))
+		return (NULL);
 	new = malloc(sizeof(listint_t));
-	if (!new || !head)
+	if (!new)
 		return (NULL);
 	if (idx == 0)
 	{
 		new->n = n;
-		new->next = head;
-		head = new;
+		new->next = *head;
+		*head = new;
 		return (new);
 	}
+	prev_node = get_nodeint_at_index(*head, (idx - 1));
+	if (!prev_node)
+		return (NULL);
 	new->n = n;
 	new->next = prev_node->next;
 	prev_node->next = new;
